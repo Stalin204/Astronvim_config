3a597b51cee3033df95bd95f4ac645fc007bf8a6
carlos <carlosf.corralesz@uqvirtual.edu.co>
2024-04-03 16:03:26 -0500

    se agrego un nuevo plugin

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,7 @@ return {
   { import = "astrocommunity.pack.typescript" },
   -- import/override with your plugins folder
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
-  { import = "astrocommunity.workflow.hardtime-nvim" },
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
